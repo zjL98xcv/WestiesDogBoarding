0c3ce4d67a0882c273581394f41e95b85074dcbe
zjL98xcv <130295317+zjL98xcv@users.noreply.github.com>
2023-06-27 23:56:07 +0800

    Add files via upload

---
+++ startbootstrap-agency-gh-pages/js/scripts.js
@@ -0,0 +1,242 @@
+/*!
+* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+
+
+
+
+
+
+    document.addEventListener('mousemove', showImgContent);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Testimonial/review */
+const reviewWrap = document.getElementById("reviewWrap");
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
+const imgDiv = document.getElementById("imgDiv");
+const personName = document.getElementById("personName");
+const profession = document.getElementById("profession");
+const description = document.getElementById("description");
+const surpriseMeBtn = document.getElementById("surpriseMeBtn");
+const chicken = document.querySelector(".chicken");
+
+let isChickenVisible;
+
+let people = [
+    {
+        photo:
+            'url("http://drive.google.com/uc?export=view&id=1Al3_zVUu7KlzOYyaqw69Vhiqu6WPvbq2")',
+        name: "Wee",
+        profession: "Kimchi's Pawrent",
+        description:
+            "Kimchi enjoyed her stay with Grace and her family. Eat until buibui!"
+    },
+
+    {
+        photo:
+            "url('http://drive.google.com/uc?export=view&id=1Ajd1DikH-BQf_C9wo6NTWMuaFQj0V2I8')",
+        name: "Jihoon",
+        profession: "Thor's Pawrent",
+        description:
+            "It was so nice to meet Grace, she was fully responsible for my dog. Pepper, her dog, was so nice and sweet. Even tho he is a big doggy, he played so well with my dog, and I loved the way how Grace treated them. She was so calm and good with big doggies. I felt so safe to let my dog stay with Grace. Definitely will book again! Thank you so much!"
+    },
+
+    {
+        photo:
+            "url('http://drive.google.com/uc?export=view&id=1B13K3KviHWLTrsJqOuS9pN8TcVSUSmJ-')",
+        name: "Namika",
+        profession: "Rusty & May's pawrent",
+        description:
+            "From the moment he came to pick up the dogs, she seemed like a very nice person and I was able to leave him with peace of mind. I'm glad you had a good time with the dog you live with. Even while I was entrusting the dogs, they sent me pictures and videos so I could see how they were doing. I want to deposit it again next time. thank you very much!"
+    },
+
+];
+
+imgDiv.style.backgroundImage = people[0].photo;
+personName.innerText = people[0].name;
+profession.innerText = people[0].profession;
+description.innerText = people[0].description;
+let currentPerson = 0;
+
+//Select the side where you want to slide
+function slide(whichSide, personNumber) {
+    let reviewWrapWidth = reviewWrap.offsetWidth + "px";
+    let descriptionHeight = description.offsetHeight + "px";
+    //(+ or -)
+    let side1symbol = whichSide === "left" ? "" : "-";
+    let side2symbol = whichSide === "left" ? "-" : "";
+
+    let tl = gsap.timeline();
+
+    if (isChickenVisible) {
+        tl.to(chicken, {
+            duration: 0.4,
+            opacity: 0
+        });
+    }
+
+    tl.to(reviewWrap, {
+        duration: 0.4,
+        opacity: 0,
+        translateX: `${side1symbol + reviewWrapWidth}`
+    });
+
+    tl.to(reviewWrap, {
+        duration: 0,
+        translateX: `${side2symbol + reviewWrapWidth}`
+    });
+
+    setTimeout(() => {
+        imgDiv.style.backgroundImage = people[personNumber].photo;
+    }, 400);
+    setTimeout(() => {
+        description.style.height = descriptionHeight;
+    }, 400);
+    setTimeout(() => {
+        personName.innerText = people[personNumber].name;
+    }, 400);
+    setTimeout(() => {
+        profession.innerText = people[personNumber].profession;
+    }, 400);
+    setTimeout(() => {
+        description.innerText = people[personNumber].description;
+    }, 400);
+
+    tl.to(reviewWrap, {
+        duration: 0.4,
+        opacity: 1,
+        translateX: 0
+    });
+
+    if (isChickenVisible) {
+        tl.to(chicken, {
+            duration: 0.4,
+            opacity: 1
+        });
+    }
+}
+
+function setNextCardLeft() {
+    if (currentPerson === 2) {
+        currentPerson = 0;
+        slide("left", currentPerson);
+    } else {
+        currentPerson++;
+    }
+
+    slide("left", currentPerson);
+}
+
+function setNextCardRight() {
+    if (currentPerson === 0) {
+        currentPerson = 2;
+        slide("right", currentPerson);
+    } else {
+        currentPerson--;
+    }
+
+    slide("right", currentPerson);
+}
+
+leftArrow.addEventListener("click", setNextCardLeft);
+rightArrow.addEventListener("click", setNextCardRight);
+
+surpriseMeBtn.addEventListener("click", () => {
+    if (chicken.style.opacity === "0") {
+        chicken.style.opacity = "1";
+        imgDiv.classList.add("move-head");
+        surpriseMeBtn.innerText = "Remove the chicken";
+        surpriseMeBtn.classList.remove("surprise-me-btn");
+        surpriseMeBtn.classList.add("hide-chicken-btn");
+        isChickenVisible = true;
+    } else if (chicken.style.opacity === "1") {
+        chicken.style.opacity = "0";
+        imgDiv.classList.remove("move-head");
+        surpriseMeBtn.innerText = "Surprise me";
+        surpriseMeBtn.classList.add("surprise-me-btn");
+        surpriseMeBtn.classList.remove("hide-chicken-btn");
+        isChickenVisible = false;
+    }
+});
+
+window.addEventListener("resize", () => {
+    description.style.height = "100%";
+});
+
+
+
+
+
+
